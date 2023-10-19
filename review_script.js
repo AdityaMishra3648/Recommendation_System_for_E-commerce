c79c8b5355cf5a488506e49376eb8bc4a45f8c11
AdityaMishra3648 <123090809+AdityaMishra3648@users.noreply.github.com>
2023-10-20 03:16:40 +0530

    .

--- review_script.js
+++ review_script.js
@@ -3,8 +3,9 @@ let text = document.querySelector("#review_text");
 let table = document.querySelector(".review_table");
 let m = document.querySelector(".review_message");
 re.addEventListener('click',()=>{
+    let val = Math.floor(Math.random() * 10)%5;
     table.innerHTML += `<tr>
-    <th><img src="A0.png" id="img"></th>
+    <th><img src="A${val}.png" id="img"></th>
     <td>${text.value}</td>
 </tr>`;
 m.innerHTML = "Thanks for Contributing your review has been added in the bottom";
