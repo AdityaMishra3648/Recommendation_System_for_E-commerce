3c99ce44036e265201e1eebc39d675ea46952c2e
AdityaMishra3648 <123090809+AdityaMishra3648@users.noreply.github.com>
2023-10-20 03:09:41 +0530

    finishing off oct 19

--- review_script.js
+++ review_script.js
@@ -1,10 +1,14 @@
 let re = document.querySelector("#review_button");
 let text = document.querySelector("#review_text");
 let table = document.querySelector(".review_table");
+let m = document.querySelector(".review_message");
 re.addEventListener('click',()=>{
-    table.innerHTML = table.innerHTML + `<tr>
+    table.innerHTML += `<tr>
     <th><img src="A0.png" id="img"></th>
     <td>${text.value}</td>
 </tr>`;
-text.value = "Review added at the bottom";
+m.innerHTML = "Thanks for Contributing your review has been added in the bottom";
+setTimeout(() => {
+    m.innerHTML = "Contribute a Review";
+}, 4000);
 });
