99bdda09130a97655013ea590fd14c0c1f993fa0
AdityaMishra3648 <123090809+AdityaMishra3648@users.noreply.github.com>
2023-10-20 15:05:51 +0530

    m

--- mail_script.js
+++ mail_script.js
@@ -5,14 +5,14 @@ function sendmail(){
         my_name : document.querySelector(".my-name").value,
         product_name : document.querySelector(".product-name").innerHTML,
         my_mail : document.querySelector(".my-mail").value,
-        seller_mail: document.querySelector(".seller-mail").innerHTML
-        
+        seller_mail: document.querySelector(".seller-mail").innerHTML,
+        mess: document.querySelector(".my-mess").value
     };
     const service_id = "service_6sxn973";
     const template_id = "template_5vclz9r";
     emailjs.send(service_id,template_id,params)
     .then(res=>{
-        
+
          document.querySelector(".my-name").value = "";
          document.querySelector(".my-mail").value = "";
         alert("request sent to the seller successfully");
